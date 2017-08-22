6bc3f09fcdbadad9764d8d3350fc56fc9f164827
Wojo <matt.wojtkun@gmail.com>
2017-08-22 08:51:56 -0400

    setup for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   WU_API = Rails.application.secrets.WU_API
 
   # In the development environment your application's code is reloaded on
